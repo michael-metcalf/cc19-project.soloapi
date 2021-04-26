a1e93c78b3624ade6f0ba669cda2f52cb16f8225
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-04-26 19:34:04 +0900

    stuck on implementing post requests

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const knex = require("./knex");
 
 const app = express();
 
 app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
-
-// app.get("/index.html", (req, res) => {
-//   res.sendFile(__dirname + "/" + "index.html");
-// });
 
 app.get("/api/ddrscores", async (req, res) => {
   const data = await knex.select().table("ddrscores");
@@ -20,13 +12,8 @@ app.get("/api/ddrscores", async (req, res) => {
   console.log("Ya got me!");
 });
 
-app.post("/api/ddrscores/addscore", urlencodedParser, async (req, res) => {
-  // const response = await { songTitle: req.body.songTitle };
-
-  const data = await knex.select().table("ddrscores");
-  res.status(200);
-  res.send("POST request called");
-  // console.log(response);
+app.post("/api/ddrscores/addscore", async (req, res) => {
+  console.log("hello world");
 });
 
 const port = 9999 || process.env.PORT;
