bd98d67a6517644d6d6fffe1fa3360b8c4cc5552
Michael Metcalf <michaelmetcalf52@gmail.com>
2021-04-26 19:16:19 +0900

    working on implementing put requests

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
 const knex = require("./knex");
 
 const app = express();
 
 app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+
+// app.get("/index.html", (req, res) => {
+//   res.sendFile(__dirname + "/" + "index.html");
+// });
 
 app.get("/api/ddrscores", async (req, res) => {
   const data = await knex.select().table("ddrscores");
   res.status(200);
   res.send(data);
   console.log("Ya got me!");
+});
+
+app.post("/api/ddrscores/addscore", urlencodedParser, async (req, res) => {
+  // const response = await { songTitle: req.body.songTitle };
+
+  const data = await knex.select().table("ddrscores");
+  res.status(200);
+  res.send("POST request called");
+  // console.log(response);
 });
 
 const port = 9999 || process.env.PORT;
